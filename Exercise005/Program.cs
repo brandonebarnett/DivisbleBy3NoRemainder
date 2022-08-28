891fcf87673331f491ffd4bdd8062d7818d6d935
Brandon <brandon.ebarnett@gmail.com>
2022-08-28 02:17:13 -0400

    Add project files.

---
+++ Exercise005/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Write a program to count how many numbers between 1 and 100 are divisible
+ *  by 3 with no remainder.  Display the count on the console. */
+
+int count = 0;
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0)
+        count++;
+}
+Console.WriteLine("Count: " + count);
